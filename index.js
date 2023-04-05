ca3256add3e3adf6b24346c055620091c6c9da91
mmk666 <129168458+mmk666@users.noreply.github.com>
2023-04-05 21:17:44 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import mock from './mock.json';
+
 const filterobj = {
   region_name: 'Japan',
   sub_region_name: 'Japan',
@@ -15,25 +17,73 @@ const categories = {
   countryRtm: ['country_name', 'rtm_val'],
 };
 
-const kpi = {
-  'ST Rptd $ Growth': ['ST_USD_AMT'],
+const mapWithKey = (arr) => {
+  return arr.map((item) => ({ key: 'measure_name', value: item }));
 };
 
-const getKPIValues = (kpi, currency) => {
+const getKpiValues = (kpi, currency) => {
   switch (kpi) {
     case 'ST Rptd $ Growth':
-      return currency === '$' ? ['ST_USD_AMT'] : ['ST_LOC_AMT'];
+      return mapWithKey(currency === '$' ? ['ST_USD_AMT'] : ['ST_LOC_AMT']);
     case 'ST Rptd YoY Units':
-      return ['ST_RPTD_QTY'];
+      return mapWithKey(['ST_RPTD_QTY']);
     case 'Quotes':
-      return [];
+      return mapWithKey([]);
     case 'AC Attach':
-      return [];
+      return mapWithKey([]);
     case 'POP':
-      return ['SOV_POP_SCORE_VA'];
+      return mapWithKey(['SOV_POP_SCORE_VA']);
     case 'Attach Access':
-      return [];
+      return mapWithKey([]);
     case 'ASP':
-      return ['ST_USD_AMT', 'ST_RPTD_QTY'];
+      return mapWithKey(['ST_USD_AMT', 'ST_RPTD_QTY']);
   }
 };
+
+const getFilteredCategories = (data, [x, y] = []) => {
+  if (y) {
+    return data?.filter(
+      (item) =>
+        (x ? item[x?.key] === x?.value : true) &&
+        (y ? item[y?.key] === y?.value : true)
+    );
+  }
+  return data?.filter((item) => (x ? item[x?.key] === x?.value : true));
+};
+
+const getFilteredKpi = (data, [x, y] = []) => {
+  if (y) {
+    return data?.filter(
+      (item) => item[x?.key] === x?.value || item[y?.key] === y?.value
+    );
+  }
+  return data?.filter((item) => item[x?.key] === x?.value);
+};
+
+/**
+ * Returns an Object
+ * @param {Array} [data]
+ * @param {string} [kpi]
+ * @param {boolean} [includePartner]
+ * @param {string} [currency]
+ * @param {obj} [filterObj]
+ * @param {string} [category]
+ */
+
+const generateData = (
+  data,
+  kpi,
+  includePartner,
+  currency,
+  filterObj,
+  category
+) => {
+  const kpival = getKpiValues(kpi);
+  const filteredKPI = getFilteredKpi(data, kpival);
+  const filteredCategory = getFilteredCategories(filteredKPI, category);
+  console.log(filteredKPI);
+};
+
+console.log(
+  generateData(mock, 'ST Rptd YoY Units', true, '', filterobj, 'region')
+);
