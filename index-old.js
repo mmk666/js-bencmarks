f0fa0974e704480b32a2207bcae41249439ef7f9
mmk666 <129168458+mmk666@users.noreply.github.com>
2023-04-07 20:04:33 +0530

    1 file modified, 1 added

--- index-old.js
+++ index-old.js
@@ -1,11 +1,11 @@
 import mock from './mock.json';
 
 const filterobj = {
-  region_name: 'Western Europe',
-  sub_region_name: 'South Europe',
-  country_name: 'Italy',
-  rtm_val: 'Consumer - Channel',
-  partner_name: 'Italy -Amazon',
+  region_name: 'US',
+  sub_region_name: 'US',
+  country_name: 'USA',
+  rtm_val: 'Telco - Channel',
+  partner_name: 'USA -AT&T',
 };
 
 /**
@@ -26,17 +26,17 @@ const getCategoryValues = (
   { region_name, sub_region_name, country_name, rtm_val }
 ) => {
   switch (type) {
-    case 'Region':
+    case 'region':
       return { region_name };
-    case 'Sub Region':
+    case 'subRegion':
       return { sub_region_name };
-    case 'Country / Geo7':
+    case 'country':
       return { country_name };
-    case 'Region RTM':
+    case 'regionRtm':
       return { region_name, rtm_val };
-    case 'Sub Region RTM':
+    case 'subRegionRtm':
       return { sub_region_name };
-    case 'Country / Geo7 RTM':
+    case 'countryRtm':
       return { country_name };
   }
 };
@@ -81,63 +81,11 @@ const getFilteredCategories = (
   return arr;
 };
 
-const getASPValue = (x, y) => {
-  const m = Math.round(x);
-  const n = Math.round(y);
-  if (m === 0 || n === 0) {
-    return 0;
-  }
-
-  return Math.round(m / n);
-};
-
-const getFilteredKpi = (data, [x, y] = [], kpi) => {
+const getFilteredKpi = (data, [x, y] = []) => {
   if (y) {
-    const arr = data?.filter(
+    return data?.filter(
       (item) => item[x?.key] === x?.value || item[y?.key] === y?.value
     );
-    if (kpi === 'ASP') {
-      const key = x?.key;
-      const m = x?.value;
-      const n = y?.value;
-      const k = arr.reduce((accum, item) => {
-        if (item[key] === m) {
-          const filterobj = {
-            region_name: item?.region_name,
-            sub_region_name: item?.sub_region_name,
-            country_name: item?.country_name,
-            rtm_val: item?.rtm_val,
-            partner_name: item?.partner_name,
-            [key]: n,
-          };
-          const obj = arr?.find((item) =>
-            Object.keys(filterobj).every((key) => item[key] === filterobj[key])
-          );
-
-          const finalObj = {
-            ...item,
-            C_MEASURE_VAL: getASPValue(item?.C_MEASURE_VAL, obj?.C_MEASURE_VAL),
-            PPPY_MEASURE_VAL: getASPValue(
-              item?.PPPY_MEASURE_VAL,
-              obj?.PPPY_MEASURE_VAL
-            ),
-            PPY_MEASURE_VAL: getASPValue(
-              item?.PPY_MEASURE_VAL,
-              obj?.PPY_MEASURE_VAL
-            ),
-            PY_MEASURE_VAL: getASPValue(
-              item?.PY_MEASURE_VAL,
-              obj?.PY_MEASURE_VAL
-            ),
-            P_MEASURE_VAL: getASPValue(item?.P_MEASURE_VAL, obj?.P_MEASURE_VAL),
-          };
-
-          accum.push(finalObj);
-        }
-        return accum;
-      }, []);
-      return k;
-    }
   }
   return data?.filter((item) => item[x?.key] === x?.value);
 };
@@ -194,13 +142,13 @@ const getWOW = (kpi, benchMarkval) => {
     case 'ST Rptd YoY Units':
       return Math.round((P_val / PY_val - 1) * 100);
     case 'Quotes - Goal Acheived':
-      return '-';
+      return '';
     case 'Apple Care Attach':
-      return '-';
+      return '';
     case 'Premium Online Placement':
       return Math.round((C_val / P_val - 1) * 100);
     case 'Attach Accessories':
-      return '-';
+      return '';
     case 'ASP':
       return Math.round((C_val / P_val - 1) * 100);
   }
@@ -231,13 +179,13 @@ const getYOY = (kpi, benchMarkval, yearType) => {
     case 'ST Rptd YoY Units':
       return Math.round((X_val / Y_val - 1) * 100);
     case 'Quotes - Goal Acheived':
-      return '-';
+      return '';
     case 'Apple Care Attach':
-      return '-';
+      return '';
     case 'Premium Online Placement':
       return Math.round((C_val / P_val - 1) * 100);
     case 'Attach Accessories':
-      return '-';
+      return '';
     case 'ASP':
       return Math.round((C_val / P_val - 1) * 100);
   }
@@ -248,37 +196,28 @@ const getMaxvalue = (arr, key) => Math.max(...arr.map((o) => o[key]));
 const getMinvalue = (arr, key) => Math.min(...arr.map((o) => o[key]));
 
 const getAvgvalue = (arr, key) =>
-  Math.round(arr.reduce((p, c) => p + c[key], 0) / arr.length);
+  arr.reduce((p, c) => p + c[key], 0) / arr.length;
 
-const getPartnerCount = (includePartner, categoryWithPartner) => {
-  const list = [
-    ...new Set(categoryWithPartner.map((item) => item?.partner_name)),
-  ];
+const getPartnerCount = (arr, includePartner) => {
+  const list = [...new Set(arr.map((item) => item?.partner_name))];
   return list.length === 0
     ? '0/0'
     : `${includePartner ? list.length : list.length - 1}/${list.length}`;
 };
 
-const getFinalObj = (
-  arr,
-  kpi,
-  benchMarkval,
-  yearType,
-  includePartner,
-  categoryWithPartner
-) => {
-  const percentageData = arr.length === 0 ? [{ value: 0 }] : arr;
+const getFinalObj = (arr, kpi, benchMarkval, yearType, includePartner) => {
+  const percentageData = getPercentageData(arr, kpi);
   const lower_range = getMinvalue(percentageData, 'value');
   const upper_range = getMaxvalue(percentageData, 'value');
   const average = getAvgvalue(percentageData, 'value');
-  const benchmark = Math.round(benchMarkval[0]?.value ?? 0);
+  const benchmark = benchMarkval[0]?.C_MEASURE_VAL ?? 0;
   const qoq = getWOW(kpi, benchMarkval);
   const qoq_color = Math.round(qoq) > -1 ? 'green' : 'red';
   const yoy = getYOY(kpi, benchMarkval, yearType);
   const yoy_color = Math.round(yoy) > -1 ? 'green' : 'red';
   const formatter = '%';
-  const partners = getPartnerCount(includePartner, categoryWithPartner);
-  const bar_color = benchmark < average ? 'red' : 'green';
+  const partners = getPartnerCount(arr, includePartner);
+  const bar_color = benchmark <= lower_range ? 'red' : 'green';
 
   return {
     kpi,
@@ -308,7 +247,7 @@ const getFinalObj = (
  * @param {string} [yearType]
  */
 
-const getKpiData = (
+const generateData = (
   data,
   kpi,
   includePartner,
@@ -318,20 +257,13 @@ const getKpiData = (
   yearType
 ) => {
   const kpival = getKpiValues(kpi, currency);
-  const filteredKPI = getFilteredKpi(data, kpival, kpi);
-  const percentageVal = getPercentageData(filteredKPI, kpi);
-  const benchMark = filterArray(percentageVal, filterObj);
+  const filteredKPI = getFilteredKpi(data, kpival);
+  const benchMark = filterArray(filteredKPI, filterObj);
   const categoryValues = getCategoryValues(category, filterObj);
   const filteredCategory = getFilteredCategories(
-    percentageVal,
+    filteredKPI,
     categoryValues,
     includePartner,
-    filterObj
-  );
-  const filteredCategoryWithPartner = getFilteredCategories(
-    percentageVal,
-    categoryValues,
-    false,
     filterObj
   );
   const finalObj = getFinalObj(
@@ -339,8 +271,7 @@ const getKpiData = (
     kpi,
     benchMark,
     yearType,
-    includePartner,
-    filteredCategoryWithPartner
+    includePartner
   );
   return finalObj;
 };
@@ -356,7 +287,7 @@ const getKpiData = (
  * @param {string} [yearType]
  */
 
-export const generateData = (
+const getKpiData = (
   data = [],
   kpi = [],
   includePartner,
@@ -365,23 +296,18 @@ export const generateData = (
   category,
   yearType
 ) => {
-  if (data) {
-    return kpi?.map((item) =>
-      getKpiData(
-        data,
-        item,
-        includePartner,
-        currency,
-        filterObj,
-        category,
-        yearType
-      )
-    );
-  }
-
-  return [];
+  return kpi?.map((item) =>
+    generateData(
+      data,
+      item,
+      includePartner,
+      currency,
+      filterObj,
+      category,
+      yearType
+    )
+  );
 };
-
 const arr = [
   'ST Rptd $ Growth Rate',
   'ST Rptd YoY Units',
@@ -391,4 +317,4 @@ const arr = [
   'Attach Accessories',
   'ASP',
 ];
-console.log(generateData(mock, arr, false, '', filterobj, 'region', 'YoY'));
+console.log(getKpiData(mock, arr, false, '', filterobj, 'region', 'YoY'));
